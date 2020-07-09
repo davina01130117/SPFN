81d3461853547d96b04360807dee434d9f8ebca6
davina01130117 <64771256+davina01130117@users.noreply.github.com>
2020-07-09 22:24:21 +0800

    Update config.sh

--- pointnet_plusplus/utils/tf_ops/config.sh
+++ pointnet_plusplus/utils/tf_ops/config.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-nvcc_bin=/usr/local/cuda-9.0/bin/nvcc
+nvcc_bin=/mnt/lustre/share/cuda-9.0/bin/nvcc
 
-cuda_include_dir=/usr/local/cuda-9.0/include
-tensorflow_include_dir=/orions3-zfs/projects/csimstu/venv3.6/lib/python3.6/site-packages/tensorflow/include
-tensorflow_external_dir=/orions3-zfs/projects/csimstu/venv3.6/lib/python3.6/site-packages/tensorflow/include/external/nsync/public
+cuda_include_dir=/mnt/lustre/share/cuda-9.0/include
+tensorflow_include_dir=/anaconda3/envs/python3.6/python3.6/site-packages/tensorflow/include
+tensorflow_external_dir=/anaconda3/envs/python3.6/python3.6/site-packages/tensorflow/include/external/nsync/public
 
-cuda_library_dir=/usr/local/cuda-9.0/lib64/
-tensorflow_library_dir=/orions3-zfs/projects/csimstu/venv3.6/lib/python3.6/site-packages/tensorflow
+cuda_library_dir=/mnt/lustre/share/cuda-9.0/lib64/
+tensorflow_library_dir=/anaconda3/envs/python3.6/python3.6/site-packages/tensorflow
 
